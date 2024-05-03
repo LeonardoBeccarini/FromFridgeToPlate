c6372253e6cf0487238f0aeca9f1b14cde4fa930
LeonardoBeccarini <leonardo.beccarini@gmail.com>
2024-05-04 01:35:09 +0200

    codesmell fix resellerDAO

--- src/main/java/com/example/fromfridgetoplate/logic/dao/ResellerDAO.java
+++ src/main/java/com/example/fromfridgetoplate/logic/dao/ResellerDAO.java
@@ -1,29 +1,24 @@
 package com.example.fromfridgetoplate.logic.dao;
 
-import com.example.fromfridgetoplate.logic.bean.OrderBean;
 import com.example.fromfridgetoplate.logic.bean.RiderBean;
 import com.example.fromfridgetoplate.logic.bean.SearchBean;
 import com.example.fromfridgetoplate.logic.exceptions.DAOException;
-import com.example.fromfridgetoplate.logic.exceptions.DbException;
 import com.example.fromfridgetoplate.logic.exceptions.OrderAssignmentException;
-import com.example.fromfridgetoplate.logic.model.Order;
 import com.example.fromfridgetoplate.logic.model.OrderList;
 import com.example.fromfridgetoplate.logic.model.Rider;
 
-import java.io.IOException;
 import java.util.List;
 
 public interface ResellerDAO {
 
     OrderList getPendingOrders(String email) throws DAOException;
 
-    void updateAvailability(OrderBean orderBean) throws DAOException ;
 
     void setAssignation(int orderId) throws OrderAssignmentException, DAOException;
 
     OrderList getAssignedOrders(String currentResellerEmail) throws DAOException;
 
-    boolean assignRiderToOrder(int orderId, int riderId) throws DAOException ;
+    void assignRiderToOrder(int orderId, int riderId) throws DAOException ;
 
     boolean isRiderAvailable(RiderBean riderBn) throws DAOException ;
 
